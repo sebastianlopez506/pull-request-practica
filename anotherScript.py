5a3d638cb1938f580427a49afa8d11bb069b3aa4
alejandro-lopez-mc <sebastian03lopez25@icloud.com>
2026-03-24 16:34:49 -0600

    Shell Rating feature added

---
+++ anotherScript.py
@@ -0,0 +1,6 @@
+print("Ratings your games")
+
+game = input("Ingrese el nombre de su juego")
+rating = input("Del 1 al 5 siendo 1 malisimo y 5 CINE ¿Qué calificación le daría?")
+
+print(f"\n\t\t\tUsted ha calificaco con {rating}★ el juego {game}")
